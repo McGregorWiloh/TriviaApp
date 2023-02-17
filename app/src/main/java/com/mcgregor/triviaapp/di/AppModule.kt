d6778c5324ef3be1b766100de8a971325112fd36
McGregor <kudziwilo@gmail.com>
2023-02-17 19:22:50 +0200

    Second commit with QuestionApi, QuestionRepository.kt, QuestionsViewModel.kt updated

--- app/src/main/java/com/mcgregor/triviaapp/di/AppModule.kt
+++ app/src/main/java/com/mcgregor/triviaapp/di/AppModule.kt
@@ -1,6 +1,7 @@
 package com.mcgregor.triviaapp.di
 
 import com.mcgregor.triviaapp.network.QuestionApi
+import com.mcgregor.triviaapp.repository.QuestionRepository
 import com.mcgregor.triviaapp.util.Constants
 import dagger.Module
 import dagger.Provides
@@ -13,6 +14,11 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideQuestionRepository(api: QuestionApi) = QuestionRepository(api)
+
     @Singleton
     @Provides
     //this question api is one and the same thing with dao, because it connects directly to get all questions
--- app/src/main/java/com/mcgregor/triviaapp/repository/QuestionRepository.kt
+++ app/src/main/java/com/mcgregor/triviaapp/repository/QuestionRepository.kt
@@ -1,11 +1,30 @@
 package com.mcgregor.triviaapp.repository
 
+import android.util.Log
+import com.mcgregor.triviaapp.data.DataOrException
 import com.mcgregor.triviaapp.model.QuestionItem
 import com.mcgregor.triviaapp.network.QuestionApi
 import javax.inject.Inject
 
 class QuestionRepository @Inject constructor(private val api: QuestionApi) {
 
-    private val listOfQuestion = ArrayList<QuestionItem>(emptyList())
+    private val dataOrException = DataOrException<ArrayList<QuestionItem>,
+            Boolean,
+            Exception>()
+
+    suspend fun getAllQuestions(): DataOrException<ArrayList<QuestionItem>, Boolean, Exception> {
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+            if(dataOrException.data.toString().isNotEmpty()){
+                dataOrException.loading = false
+            }
+
+        } catch (exception: Exception) {
+            dataOrException.e = exception
+            Log.d("Exc", "getAllQuestions: ${dataOrException.e!!.localizedMessage}")
+        }
+        return dataOrException
+    }
 
 }
