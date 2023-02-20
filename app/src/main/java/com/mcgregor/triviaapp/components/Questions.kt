84a126066a10bdb0788e32fae59ef1cef7c5113c
McGregor <kudziwilo@gmail.com>
2023-02-20 20:48:05 +0200

    added next question button and answer states and QuestionTracker composable now reflects the current question

--- app/src/main/java/com/mcgregor/triviaapp/components/Questions.kt
+++ app/src/main/java/com/mcgregor/triviaapp/components/Questions.kt
@@ -3,6 +3,7 @@ package com.mcgregor.triviaapp.components
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -33,16 +34,29 @@ import com.mcgregor.triviaapp.viewmodel.QuestionsViewModel
 fun Questions(viewModel: QuestionsViewModel) {
 
     val questions = viewModel.data.value.data?.toMutableList()
-
+    val questionIndex = remember {
+        mutableStateOf(0)
+    }
     if (viewModel.data.value.loading == true) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
             CircularProgressIndicator()
         }
 
 
     } else {
+        val question = try {
+            questions?.get(questionIndex.value)
+        } catch (e: Exception){
+            null
+        }
         if (questions != null) {
-            QuestionDisplay(question = questions.first())
+            QuestionDisplay(question = question!!, questionIndex, viewModel) {
+                questionIndex.value = questionIndex.value + 1
+            }
+
         }
     }
 
@@ -51,9 +65,9 @@ fun Questions(viewModel: QuestionsViewModel) {
 @Composable
 fun QuestionDisplay(
     question: QuestionItem,
-    //questionIndex: MutableState<Int>,
-    //viewModel: QuestionsViewModel,
-    onNextClicked: (Int) -> Unit = {}
+    questionIndex: MutableState<Int>,
+    viewModel: QuestionsViewModel,
+    onNextClicked: (Int) -> Unit
 ) {
     val choicesState = remember(question) { question.choices.toMutableList() }
     //holds the index of the answer selected from the mutable list above
@@ -72,15 +86,15 @@ fun QuestionDisplay(
     val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
     Surface(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(4.dp), color = AppColors.mDarkPurple
+            .fillMaxSize(),
+        color = AppColors.mDarkPurple
     ) {
         Column(
             modifier = Modifier.padding(12.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start
         ) {
-            QuestionTracker(counter = 1, outOf = 100)
+            QuestionTracker(counter = questionIndex.value+1, outOf = 100)
             DrawDottedLine(pathEffect = pathEffect)
 
             Column {
@@ -98,11 +112,13 @@ fun QuestionDisplay(
 
                 //choices
                 choicesState.forEachIndexed { index, answerText ->
+                    //make whole row clickable to be able select your answer if you click anywhere within the row
                     Row(
                         modifier = Modifier
+                            .clickable { updateAnswer(index) }
                             .padding(3.dp)
                             .fillMaxWidth()
-                            .height(45.dp)
+                            .height(IntrinsicSize.Min) //gives each row a height to fill all the text, not to cut it
                             .border(
                                 width = 4.dp,
                                 brush = Brush.linearGradient(
@@ -133,10 +149,47 @@ fun QuestionDisplay(
                                     Color.Green.copy(alpha = 0.2f)
                                 } else {
                                     Color.Red.copy(alpha = 0.2f)
-                                }))
-                        Text(text = answerText)
+                                }
+                            )
+                        )
+                        val annotatedString = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    fontWeight = FontWeight.Light,
+                                    color = if (correctAnswerState.value == true && index == answerState.value) {
+                                        Color.Green
+                                    } else if (correctAnswerState.value == false && index == answerState.value) {
+                                        Color.Red
+                                    } else {
+                                        AppColors.mOffWhite
+                                    }, fontSize = 17.sp
+                                )
+                            ) {
+                                append(answerText)
+                            }
+
+                        }
+                        Text(text = annotatedString, modifier = Modifier.padding(6.dp))
+
                     }
+
                 }
+                Button(
+                    onClick = { onNextClicked(questionIndex.value) },
+                    modifier = Modifier
+                        .padding(top = 16.dp, start = 3.dp, end = 3.dp, bottom = 3.dp)
+                        .align(alignment = Alignment.CenterHorizontally),
+                    shape = RoundedCornerShape(34.dp),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = AppColors.mLightBlue)
+                ) {
+                    Text(
+                        text = "Next Question",
+                        modifier = Modifier.padding(4.dp),
+                        color = AppColors.mOffWhite,
+                        fontSize = 17.sp
+                    )
+                }
+
 
             }
         }
